9d2e686463bc5d6a72a440985c1b8b9597adb15f
dong <sink0324@naver.com>
2024-11-04 23:49:52 +0900

    [ADD] 초기 설정 기입 -자바 17과 gradle로 build 하였으며, 환경설정 yml로 변경 -DB 연결 안 됨

---
+++ src/test/java/nova/dding/DdingApplicationTests.java
@@ -0,0 +1,13 @@
+package nova.dding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
