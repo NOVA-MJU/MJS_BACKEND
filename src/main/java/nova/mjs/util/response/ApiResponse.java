b177cccb9de31e85183f78853b4b903ce29c2e68
hyunbin1 <kimgusqls1@gmail.com>
2025-01-04 22:04:43 +0900

    [ADD] 응답 일관화를 위하여 API response 추가

---
+++ src/main/java/nova/mjs/util/response/ApiResponse.java
@@ -0,0 +1,20 @@
+package nova.mjs.util.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private String status;              // HTTP 상태 코드
+    private T data;                  // 실제 데이터
+    private LocalDateTime timestamp; // 응답 시간
+
+    // 성공 응답
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>("success", data, LocalDateTime.now());
+    }
+}
