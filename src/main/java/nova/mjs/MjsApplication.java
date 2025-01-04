167ba11bb1235be7ce7b4af304de1fd035dea3a9
hyunbin1 <kimgusqls1@gmail.com>
2025-01-04 17:12:29 +0900

    [fix] 폴더 구조 정상화

--- src/main/java/nova/mjs/MjsApplication.java
+++ src/main/java/nova/mjs/MjsApplication.java
@@ -1,14 +1,14 @@
-package nova.dding;
+package nova.mjs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //절대 바꾸면 안 됨(이름을) - 모든 프로젝트의 근간이 되는 class
 @SpringBootApplication
-public class DdingApplication {
+public class MjsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DdingApplication.class, args);
+        SpringApplication.run(MjsApplication.class, args);
     }
 
 }
