79a08cc7cf8e7f8801bdd12292be83145708ab5d
남보라 <nambola@nambolaui-MacBookAir.local>
2025-01-27 03:37:23 +0900

    fetch errorcode

--- src/main/java/nova/mjs/util/exception/ErrorCode.java
+++ src/main/java/nova/mjs/util/exception/ErrorCode.java
@@ -11,7 +11,6 @@ public enum ErrorCode {
     API_UNKNOWN_FINISH_REASON(HttpStatus.INTERNAL_SERVER_ERROR, "API_UNKNOWN_FINISH_REASON", "[MJS] 알 수 없는 이유로 응답을 불러올 수 없습니다."),
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DATABASE_ERROR", "[MJS] 데이터베이스 오류가 발생했습니다."),
 
-
     // 요청 에러 (R)
     INVALID_PARAM_REQUEST(HttpStatus.BAD_REQUEST, "INVALID_PARAM_REQUEST", "[MJS] 요청된 파람 값이 잘못되었습니다."),
     OFFSET_IS_LESS_THEN_ONE(HttpStatus.BAD_REQUEST, "OFFSET_IS_LESS_THEN_ONE", "[MJS] offset은 1부터 시작합니다."),
@@ -19,11 +18,12 @@ public enum ErrorCode {
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "INVALID_INPUT", "[MJS] 잘못된 입력입니다."),
     INVALID_S3_URL(HttpStatus.BAD_REQUEST, "INVALID_S3_URL", "[MJS] 유효하지 않은 S3 Url입니다."),
 
-    // 자유 게시판 관련 에러(C)
-    COMMUNITY_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMUNITY_NOT_FOUND", "[MJS] 요청한 게시판을 찾을 수 없습니다."),
-
     // 공지사항 에러 관련 추가 코드
-    NOT_FOUND(HttpStatus.NOT_FOUND, "NOT_FOUND", "[MJS] 해당 조건의 공지사항을 찾을 수 없습니다.");
+    NOT_FOUND(HttpStatus.NOT_FOUND, "NOT_FOUND", "[MJS] 해당 조건의 공지사항을 찾을 수 없습니다."),
+
+    // 자유 게시판 관련 에러(C)
+    COMMUNITY_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMUNITY_NOT_FOUND", "[MJS] 요청한 게시판을 찾을 수 없습니다.")
+    ;
 
 
 
