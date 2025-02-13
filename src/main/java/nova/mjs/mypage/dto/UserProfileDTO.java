bfef461a71cfffe3a0d191003788f783779e8a5b
hyunbin1 <kimgusqls1@gmail.com>
2025-02-13 17:45:27 +0900

    [FIX] 파일명을 대문자로 변경

--- src/main/java/nova/mjs/mypage/dto/UserProfileDTO.java
+++ src/main/java/nova/mjs/mypage/dto/UserProfileDTO.java
@@ -4,19 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import nova.mjs.mypage.entity.User;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-
-public class UserProfileDto {
+public class UserProfileDTO {
     private String nickname;
     private String email;
     private String profileImage;
 
-    public static UserProfileDto fromEntity(nova.mjs.mypage.entity.User user) {
-        return UserProfileDto.builder()
+    public static UserProfileDTO fromEntity(User user) {
+        return UserProfileDTO.builder()
                 .nickname(user.getNickname())
                 .email(user.getEmail())
                 .profileImage(user.getProfileImage())
