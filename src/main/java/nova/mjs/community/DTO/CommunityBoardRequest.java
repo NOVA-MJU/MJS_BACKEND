69e251fc65fb5c3852ef0ddb84283026175379dc
namtaera <skaqhfk00@gmail.com>
2025-03-11 16:37:35 +0900

    [Modify] delete method

--- src/main/java/nova/mjs/community/DTO/CommunityBoardRequest.java
+++ src/main/java/nova/mjs/community/DTO/CommunityBoardRequest.java
@@ -16,15 +16,4 @@ public class CommunityBoardRequest {
     private List<String> contentImages; // 게시글 이미지 리스트
     private Integer likeCount;
 
-    // category는 무조건 FREE로 설정
-    public CommunityBoard toEntity() {
-        return CommunityBoard.create(
-                this.title,
-                this.content,
-                CommunityCategory.FREE, // 무조건 FREE 설정
-                this.published != null ? this.published : false,
-                this.contentImages,
-                this.likeCount != null ? this.likeCount : 0
-        );
-    }
 }
