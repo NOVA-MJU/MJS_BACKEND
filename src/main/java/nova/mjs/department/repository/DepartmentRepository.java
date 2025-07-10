ee96bd654994d3e057840f24950fc32abeea2457
dong <sink0324@naver.com>
2025-07-11 01:46:48 +0900

    [FIX] 레포지토리 메서드 수정

--- src/main/java/nova/mjs/department/repository/DepartmentRepository.java
+++ src/main/java/nova/mjs/department/repository/DepartmentRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    Optional<Department> findByName(String name);
+    Optional<Department> findByDepartmentName(String departmentName);
 
     Optional<Department> findByDepartmentUuid(UUID uuid);
 
