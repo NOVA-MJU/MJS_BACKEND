72cdccd72e4681f3569a4acd879bb978881a8ffb
namtaera <skaqhfk00@gmail.com>
2025-07-16 00:46:10 +0900

    [ADD] department main, department notice

--- src/main/java/nova/mjs/domain/department/repository/DepartmentRepository.java
+++ src/main/java/nova/mjs/domain/department/repository/DepartmentRepository.java
@@ -1,8 +1,10 @@
 package nova.mjs.domain.department.repository;
 
 import nova.mjs.domain.department.entity.Department;
+import nova.mjs.domain.member.entity.enumList.College;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,5 +12,7 @@ public interface DepartmentRepository extends JpaRepository<Department, Long> {
     Optional<Department> findByDepartmentName(String departmentName);
 
     Optional<Department> findByDepartmentUuid(UUID uuid);
+
+    List<Department> findByCollege(College college);
 
 }
