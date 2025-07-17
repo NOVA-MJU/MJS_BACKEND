299a1079d26a54591e1343bedbee9a6956015ea6
dong <sink0324@naver.com>
2025-07-17 21:56:02 +0900

    [FIX] 실시간 검색어 메서드 메인 검색 컨트롤러에 추가

--- src/main/java/nova/mjs/util/ElasticSearch/CombinedSearchController.java
+++ src/main/java/nova/mjs/util/ElasticSearch/CombinedSearchController.java
@@ -1,6 +1,7 @@
 package nova.mjs.util.ElasticSearch;
 
 import lombok.RequiredArgsConstructor;
+import nova.mjs.domain.realtimeKeyword.RealtimeKeywordService;
 import nova.mjs.util.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import java.util.List;
 @RequestMapping("/api/v1/search")
 public class CombinedSearchController {
     private final CombinedSearchService combinedSearchService;
+    private final RealtimeKeywordService realtimeKeywordService;
 
     @PostMapping("/sync")
     public ResponseEntity<ApiResponse<String>> syncElasticsearch() {
@@ -30,6 +32,9 @@ public class CombinedSearchController {
             @RequestParam(defaultValue = "10") int size) {
 
         List<SearchResponseDTO> results = combinedSearchService.unifiedSearch(keyword, type, page, size);
+
+        realtimeKeywordService.recordSearch(keyword);
+
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ApiResponse.success(results));
