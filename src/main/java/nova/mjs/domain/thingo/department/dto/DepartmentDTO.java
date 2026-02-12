16a461d2e761de37ef6da45390c5f9d4393dfdb3
hyunbin1 <kimgusqls1@gmail.com>
2026-02-12 17:33:21 +0900

    [FIX] 학과 불필요한 연관관계 삭제 및 단과대, 학과 컬럼 추가 + 이에 따른 메서드 변경

---
+++ src/main/java/nova/mjs/domain/thingo/department/dto/DepartmentDTO.java
@@ -0,0 +1,57 @@
+package nova.mjs.domain.thingo.department.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import nova.mjs.domain.thingo.department.entity.Department;
+import nova.mjs.domain.thingo.department.entity.enumList.College;
+import nova.mjs.domain.thingo.department.entity.enumList.DepartmentName;
+
+public class DepartmentDTO {
+
+    /* ==================================================
+     * 생성 요청 DTO
+     * ================================================== */
+    @Getter
+    public static class CreateRequest {
+
+        private College college;
+        private DepartmentName departmentName;
+        private String academicOfficePhone;
+        private String instagramUrl;
+        private String homepageUrl;
+    }
+
+    /* ==================================================
+     * 수정 요청 DTO
+     * ================================================== */
+    @Getter
+    public static class UpdateRequest {
+        private College college;
+        private DepartmentName departmentName;
+        private String academicOfficePhone;
+        private String instagramUrl;
+        private String homepageUrl;
+    }
+
+    /* ==================================================
+     * 정보 조회 응답 DTO (단건)
+     * ================================================== */
+    @Getter
+    @Builder
+    public static class InfoResponse {
+
+        private College college;
+        private DepartmentName departmentName;
+        private String instagramUrl;
+        private String homepageUrl;
+
+        public static InfoResponse fromEntity(Department department) {
+            return InfoResponse.builder()
+                    .college(department.getCollege())
+                    .departmentName(department.getDepartmentName())
+                    .instagramUrl(department.getInstagramUrl())
+                    .homepageUrl(department.getHomepageUrl())
+                    .build();
+        }
+    }
+}
--- src/main/java/nova/mjs/domain/thingo/department/entity/enumList/DepartmentName.java
+++ src/main/java/nova/mjs/domain/thingo/department/entity/enumList/DepartmentName.java
@@ -1,11 +1,11 @@
-package nova.mjs.domain.thingo.member.entity.enumList;
+package nova.mjs.domain.thingo.department.entity.enumList;
 
 import lombok.Getter;
 
 @Getter
 public enum DepartmentName {
 
-    HUMANITIES_COLLEGE("인문대"),       // 인문대 <단과대>
+//    HUMANITIES_COLLEGE("인문대"),       // 인문대 <단과대>
     CHINESE_LITERATURE("중어중문학과"),
     JAPANESE_LITERATURE("일어일문학과"),
     ARABIC_STUDIES("아랍지역학과"),
@@ -18,7 +18,7 @@ public enum DepartmentName {
     CULTURAL_CONTENT_STUDIES("글로벌문화콘텐츠학전공"),
     PHILOSOPHY("철학과"),
 
-    SOCIAL_SCIENCES("사회과학대학"),      // 사과대 <단과대>
+//    SOCIAL_SCIENCES("사회과학대학"),      // 사과대 <단과대>
     PUBLIC_ADMINISTRATION("행정학과"),
     POLITICAL_DIPLOMACY("정치외교학과"),
     LAW("법학과"),
@@ -26,25 +26,25 @@ public enum DepartmentName {
     INTERNATIONAL_TRADE("국제통상학전공"), // -> 홈피엔 경영대에 있는데 그거 사라지고 사과대로 갔다 함
     APPLIED_STATISTICS("응용통계학과"),
 
-    BUSINESS("경영대학"),               // 경영대 <단과대>
+//    BUSINESS("경영대학"),               // 경영대 <단과대>
     BUSINESS_ADMINISTRATION("경영학과"),
     GLOBAL_BUSINESS_STUDIES("글로벌비즈니스학과"),
     MANAGEMENT_INFORMATION_SYSTEMS("경영정보학과"),
 
-    MEDIA_HUMANLIFE("미디어·휴먼라이프대학"),// 미디어휴먼라이프대 <단과대>
+//    MEDIA_HUMANLIFE("미디어·휴먼라이프대학"),// 미디어휴먼라이프대 <단과대>
     DIGITAL_MEDIA_STUDIES("디지털미디어학부"),
     YOUTH_GUIDANCE_STUDIES("청소년지도학과"),
     CHILD_STUDIES("아동학과"),
 
-    AI_SOFTWARE("인공지능소프트웨어융합대학"), //인소대 <단과대>
+//    AI_SOFTWARE("인공지능소프트웨어융합대학"), //인소대 <단과대>
     CONVERGENT_SOFTWARE_STUDIES("융합소프트웨어학부"),
     DIGITAL_CONTENT_DESIGN_STUDIES("디지털콘텐츠디자인학과"),
     DATA_SCIENCE("데이터사이언스학과"),
     APPLICATION_SOFTWARE("응용소프트웨어학과"),
 
-    FUTURE_CONVERGENCE("미래융합대학"),     //미융대 <단과대>
+//    FUTURE_CONVERGENCE("미래융합대학"),     //미융대 <단과대>
 
-    HONOR("아너칼리지(자유전공학부)"),         //아너칼리지 <단과대> 자유전공학부
+//    HONOR("아너칼리지(자유전공학부)"),         //아너칼리지 <단과대> 자유전공학부
 
     OTHER("기타");
 
